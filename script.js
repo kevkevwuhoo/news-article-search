33cceef913eb0d71594a68787c4b19966562fe58
kwu3 <kwu3@villanova.edu>
2020-04-14 23:35:31 -0400

    Added user input to api request

--- script.js
+++ script.js
@@ -9,30 +9,78 @@ var apikey = "byMMQjlz1JtTyUnGQ2hwhL16rPEYaPDo";
 // "#well-section" articles div
 
 // TODO:
-// replace hard coded values
-// limit number of articles
+// what if user doesn't specify begin or end year
+// make sure none of the response data is null
 
 // user search
-var search = "election";
-var beginYear = "2010";
-var endYear = "2020";
+var search = "";
+var numberResults = 0;
+var beginYear = 0;
+var endYear = 0;
 
-// ajax request
-queryURL =
-    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-    search +
-    "&api-key=" +
-    apikey +
-    "&begin_date=" +
-    beginYear +
-    "0101" +
-    "&end_date=" +
-    endYear +
-    "1231";
+// function runSearch (numArticles)
+function runSearch(numArticles) {
+    // ajax request
+    var queryURL =
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+        search +
+        "&api-key=" +
+        apikey +
+        "&begin_date=" +
+        beginYear +
+        "0101" +
+        "&end_date=" +
+        endYear +
+        "1231";
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function (response) {
-    console.log(response);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (nytresponse) {
+        console.log(nytresponse);
+        // create article divs to house article information
+        for (var i = 0; i < numArticles; i++) {
+            // new article element
+            var articleDiv = $("<div>");
+            // get title of article
+            var title = $("<p>").text(nytresponse.response.docs[i].headline.main);
+            console.log(nytresponse.response.docs[i].headline.main);
+            // add title to article element
+            articleDiv.append(title);
+            // get author of article
+            var author = $("<p>").text(nytresponse.response.docs[i].byline.original);
+            console.log(nytresponse.response.docs[i].byline.original);
+            // add author to article element
+            articleDiv.append(author);
+            // add article element to well-section
+            $("well-section").append(articleDiv);
+        }
+    });
+}
+
+// clear the articles on the page
+function clearArticles() {
+    // clear existing articles
+    $("#well-section").empty();
+}
+
+// search button on click listener
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
+    // clear existing articles
+    clearArticles();
+    // get search value, trim white space
+    search = $("#search-term").val().trim();
+    // get number of search results
+    numberResults = $("#num-records-select").val().trim();
+    // get start year
+    beginYear = $("#start-year").val().trim();
+    // get end year
+    endYear = $("#end-year").val().trim();
+
+    // do search
+    runSearch(numberResults);
 });
+
+// clear button on click listener
+$("#clear-all").on("click", clearArticles);
